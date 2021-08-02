cde173457a4eca9d97dcd11ccbf87d0afdc68657
Obafemi Joseph <obafedance@gmail.com>
2021-08-03 00:19:27 +0100

    adds more test coverage fixes #2

--- test/unit tests/unitest.test.js
+++ test/unit tests/unitest.test.js
@@ -1,23 +1,56 @@
 /* eslint-disable no-undef */
-const { handleGit, handleError, handleHelp } = require('../../lib/utils/parseFirstArg.js');
+const {
+  handleGit,
+  handleError,
+  handleHelp,
+  stripInput,
+  parseArgv
+} = require('../../lib/utils/index.js');
 
-describe('HANDLE FIRST ARGV', () => {
-  it('Should return same input if passed', () => {
-    const returnedValue = handleGit('sample');
-    expect(returnedValue).not.toBeUndefined();
-    expect(returnedValue).toEqual('sample');
+describe('UTILS', () => {
+  describe('HANDLE FIRST ARGV', () => {
+    it('Should return same input if passed', () => {
+      const returnedValue = handleGit('sample');
+      expect(returnedValue).not.toBeUndefined();
+      expect(returnedValue).toEqual('sample');
+    });
+    it('Should should return error text if input is not git or help', () => {
+      const returnedValue = handleError('hello');
+
+      expect(returnedValue).not.toBeUndefined();
+      expect(returnedValue).toContain('hello');
+    });
+
+    it('should return help text when called', () => {
+      const returnedValue = handleHelp();
+
+      expect(returnedValue).toContain('askgit');
+      expect(returnedValue).not.toBeUndefined();
+    });
   });
-  it('Should should return error text if input is not git or help', () => {
-    const returnedValue = handleError('hello');
+  describe('STRIP GIVEN INPUT ', () => {
+    let output;
+    it('should return custom message if input is undefined or empty', () => {
+      output = `A flag is missing a parameter, check to see if you need to pass a filename, commit message or some other parameter `;
+      const returnedValue = stripInput('');
+      expect(returnedValue).toEqual(output);
+    });
 
-    expect(returnedValue).not.toBeUndefined();
-    expect(returnedValue).toContain('hello');
+    it('should return custom message if input is white spaces', () => {
+      output = `You can't pass an empty filename or whitespaces`;
+      const returnedValue = stripInput(' ');
+      expect(returnedValue).toEqual(output);
+    });
+
+    it('should return the input if input passed is not undefined or whitespaces', () => {
+      const returnedValue = stripInput('sample');
+      expect(returnedValue).toEqual('sample');
+    });
   });
-
-  it('should return help text when called', () => {
-    const returnedValue = handleHelp();
-
-    expect(returnedValue).toContain('askgit');
-    expect(returnedValue).not.toBeUndefined();
+  describe('HANDLE PARSING OF ARGVS', () => {
+    it('should always return the fourth element of an array', () => {
+      const returnedValue = parseArgv(['0', '1', '2', '3', '4']);
+      expect(returnedValue).toEqual('3');
+    });
   });
 });
